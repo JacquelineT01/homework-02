ed19063538a0e7d96b75782525ef34ceffcd26a7
Jacqueline <tiessijacqueline@yahoo.com>
2023-09-24 00:35:00 +0300

    Initial commit

---
+++ src/MyClass.java
@@ -0,0 +1,17 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int n = 6;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 3 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
